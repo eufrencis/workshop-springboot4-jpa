e4965a3b166bea85335371ed04c1357493952896
Francis Ferreira <francis_patricio_@hotmail.com>
2026-01-23 11:15:36 -0300

    feat: implement custom OrderStatus with manual code mapping

--- course/src/main/java/com/example/course/entities/Order.java
+++ course/src/main/java/com/example/course/entities/Order.java
@@ -3,6 +3,7 @@ package com.example.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.example.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 
@@ -28,6 +29,7 @@ public class Order implements Serializable {
     timezone = "GMT" // 3. Garante que o horário seja o de Londres (UTC/Zero)
     )   
     private Instant moment;
+    private Integer orderStatus;
    
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -35,9 +37,10 @@ public class Order implements Serializable {
 
     public Order (){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus (orderStatus);
         this.client = client;
     }
 
@@ -63,6 +66,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+        this.orderStatus = orderStatus.getCode();
+    }
     }
 
     @Override
