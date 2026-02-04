df9e5ab025e5f9aa0499dd0b043f53142c6bf4fa
Francis Ferreira <francis_patricio_@hotmail.com>
2026-02-04 14:38:29 -0300

    feat: User update

--- course/src/main/java/com/example/course/services/UserService.java
+++ course/src/main/java/com/example/course/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.course.entities.User;
 import com.example.course.repositories.UserRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -34,13 +35,20 @@ public class UserService {
         repository.deleteById(id);
     }
 
+    @Transactional
+    public User update (Long id, User obj){
+        User entity = repository.getReferenceById(id); // findbyId vai no bando de dados e tras o objeto o referenc so prepara o objeto monitorado pra alterar e dps mudar o banco de dados
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
 
-
-
-
-
-
-
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
 
 
     }
+
+
+}
